1757b447066e5984a51dbceabf970d1b154e2233
Alex de Carvalho <leco_123carvalho@hotmail.com>
2022-08-05 06:48:41 -0300

    2.25. Criando e acessando propriedades customizadas com @Value

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.di.notificacao;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import com.algaworks.algafood.di.modelo.Cliente;
@@ -9,28 +10,14 @@ import com.algaworks.algafood.di.modelo.Cliente;
 @Component
 public class NotificadorEmail implements Notificador {
 	
-	//private boolean caixaAlta;
-	//private String hostServidorSmtp;
-	/*
-	public NotificadorEmail(String hostServidorSmtp) {
-		this.hostServidorSmtp = hostServidorSmtp;
-		System.out.println("NotificadorEmail");
-	}
-	*/
+	// Pegando valor de uma propriedade
+	@Value("${minha.propriedade-teste}")
+	private String host;
 	
 	@Override
 	public void notificar(Cliente cliente, String mensagem) {
-		//if(this.caixaAlta) {
-			//mensagem = mensagem.toUpperCase();
-		//}
-		System.out.printf("Notificando %s através do e-mail %s: usando SMTP: %s\n", 
+		System.out.printf("Pegando valor de uma propriedade: minha.propriedade-teste = %s \n", host);
+		System.out.printf("Notificando %s através do e-mail %s: usando SMTP: %s \n", 
 				cliente.getNome(), cliente.getEmail(), mensagem);
 	}
-	
-	/*
-	public void setCaixaAlta(boolean caixaAlta) {
-		this.caixaAlta = caixaAlta;
-	}
-	*/
-
 }
