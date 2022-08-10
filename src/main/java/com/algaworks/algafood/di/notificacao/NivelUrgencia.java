40a25078b0b4a1ad4d51c73b26a1602862af6cf4
Alex de Carvalho <leco_123carvalho@hotmail.com>
2022-08-10 16:56:56 -0300

    REMOVENDO EXPLICAÇÕES E COMEÇANDO A DESENVOLVER O PROJETO DO ZERO

--- src/main/java/com/algaworks/algafood/di/notificacao/NivelUrgencia.java
+++
@@ -1,6 +0,0 @@
-package com.algaworks.algafood.di.notificacao;
-
-public enum NivelUrgencia {
-	URGENTE,
-	SEM_URGENCIA
-}
--- src/main/java/com/algaworks/algafood/di/notificacao/Notificador.java
+++
@@ -1,7 +0,0 @@
-package com.algaworks.algafood.di.notificacao;
-
-import com.algaworks.algafood.di.modelo.Cliente;
-
-public interface Notificador {
-	void notificar(Cliente cliente, String mensagem);
-}
