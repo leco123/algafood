17f9e03b9739fbdf6674ff94c43fcba739763644
Alex de Carvalho <leco_123carvalho@hotmail.com>
2022-08-15 15:35:08 -0300

    4.25. Modelando e implementando a atualização de recursos com PUT
    Existe duas maneiras de implementar put
    -1º Setando todos os parâmetros
    -2º recomendado usar a class BeanUtils.copyProperties que faz a cópia das propriedades cozinha e adicionar no cozinhaAtual.

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -2,14 +2,15 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -46,9 +47,22 @@ public class CozinhaController {
 		return ResponseEntity.notFound().build();
 	}
 	
-	@PostMapping(consumes = MediaType.APPLICATION_XML_VALUE)
+	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cozinha salvar(@RequestBody Cozinha cozinha) {
 		return cozinhaRepository.adicionar(cozinha);
+	}
+	
+	@PutMapping("/{cozinhaid}")
+	public ResponseEntity<Cozinha> atualizar(@PathVariable("cozinhaid") Long id, @RequestBody Cozinha cozinha) {
+		Cozinha cozinhaAtual = cozinhaRepository.porId(id);
+		// Existe duas maneiras de implementar put
+		// 1º Setando todos os parâmetros
+		cozinhaAtual.setNome(cozinha.getNome());
+		// 2º recomendado usar a class BeanUtils.copyProperties
+		// faz a cópia das propriedades cozinha e adicionar no cozinhaAtual.
+		BeanUtils.copyProperties(cozinha, cozinhaAtual);
+		cozinhaRepository.adicionar(cozinhaAtual);
+		return ResponseEntity.ok(cozinhaAtual);
 	}
 }
