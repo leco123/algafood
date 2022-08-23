dbd6058a6484bbca816a4bcab12e8ad3a0ff1501
Alex de Carvalho <leco_123carvalho@hotmail.com>
2022-08-22 22:17:26 -0300

    5.14. Adicionando restrições na cláusula where com Criteria API

--- src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -9,6 +9,8 @@ import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -23,7 +25,15 @@ public class RestauranteRepositoryImpl implements RestauranteRepositoryQueries {
 
         CriteriaBuilder builder = manager.getCriteriaBuilder();
         CriteriaQuery<Restaurante> criteria = builder.createQuery(Restaurante.class);
-        criteria.from(Restaurante.class);
+        // root representa a raiz do restaurante
+        Root<Restaurante> root =  criteria.from(Restaurante.class);
+
+        //Predicado
+        Predicate nomePredicate = builder.like(root.get("nome"), "%"+nome+"%");
+        Predicate taxaInicialPredicate = builder.greaterThanOrEqualTo(root.get("taxaFrete"), taxaFreteInicial);
+        Predicate taxaFinalPredicate = builder.lessThanOrEqualTo(root.get("taxaFrete"), taxaFreteFinal);
+
+        criteria.where(nomePredicate, taxaInicialPredicate, taxaFinalPredicate);
 
         TypedQuery<Restaurante> query = manager.createQuery(criteria);
         return query.getResultList();
