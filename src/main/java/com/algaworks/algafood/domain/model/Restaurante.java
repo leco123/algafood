ace90389a6a4eaa5cc053be0508b4cca30b4cbd0
Alex de Carvalho <leco_123carvalho@hotmail.com>
2022-08-29 20:50:52 -0300

    6.10. Entendendo o Eager Loading

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -28,6 +28,7 @@ public class Restaurante {
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 
+	@JsonIgnore
 	@ManyToOne
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
