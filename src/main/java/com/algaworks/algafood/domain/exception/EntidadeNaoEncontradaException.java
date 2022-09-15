24e7f63234ba93c3062717d66884688543f85788
Alex de Carvalho <leco_123carvalho@hotmail.com>
2022-09-14 23:21:47 -0300

    8.4. Estendendo ResponseStatusException

--- src/main/java/com/algaworks/algafood/domain/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/algaworks/algafood/domain/exception/EntidadeNaoEncontradaException.java
@@ -1,14 +1,17 @@
 package com.algaworks.algafood.domain.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class EntidadeNaoEncontradaException extends RuntimeException {
+public class EntidadeNaoEncontradaException extends ResponseStatusException {
 
     private static final long serialVersionUID = 1L;
 
+    public EntidadeNaoEncontradaException(HttpStatus status, String mensagem) {
+        super(status, mensagem);
+    }
+
     public EntidadeNaoEncontradaException(String mensagem) {
-        super(mensagem);
+        this(HttpStatus.NOT_FOUND, mensagem);
     }
 }
--- src/main/java/com/algaworks/algafood/domain/service/CadastroCozinhaService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroCozinhaService.java
@@ -7,9 +7,7 @@ import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class CadastroCozinhaService {
@@ -25,11 +23,9 @@ public class CadastroCozinhaService {
 		try {
 			cozinhaRepository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
-			throw new ResponseStatusException(HttpStatus.NOT_FOUND,
-					String.format("Não existe um cadastro de cozinha com código %d ", id));
-		/*	throw  new EntidadeNaoEncontradaException(
+			throw new EntidadeNaoEncontradaException(
 					String.format("Não existe um cadastro de cozinha com código %d ", id)
-			);*/
+			);
 		} catch (DataIntegrityViolationException e) {
 			throw  new EntidadeEmUsoException(
 					String.format("Cozinha de código %d não pode ser removida, pois está em uso", id)
