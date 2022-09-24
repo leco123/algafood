7036747046d8bb2fafb39989c63366153d2108a4
Alex de Carvalho <leco_123carvalho@hotmail.com>
2022-09-24 18:34:27 -0300

    8.12. Tratando exceções em nível de controlador com @ExceptionHandler

--- src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CidadeController.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.controller;
 
+import com.algaworks.algafood.api.exceptionhandler.Problema;
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.EstadoNaoEncontradoException;
@@ -14,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -58,5 +60,21 @@ public class CidadeController {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void remover(@PathVariable Long cidadeId) {
 		cadastroCidade.excluir(cidadeId);
+	}
+
+    @ExceptionHandler(EntidadeNaoEncontradaException.class)
+    public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensage(e.getMessage()).build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
+    }
+
+	@ExceptionHandler(NegocioException.class)
+	public ResponseEntity<?> tratarNegocioException(NegocioException e) {
+		Problema problema = Problema.builder()
+				.dataHora(LocalDateTime.now())
+				.mensage(e.getMessage()).build();
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
 	}
 }
---
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/Problema.java
@@ -0,0 +1,15 @@
+package com.algaworks.algafood.api.exceptionhandler;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class Problema {
+
+    private LocalDateTime dataHora;
+    private String mensage;
+
+}
