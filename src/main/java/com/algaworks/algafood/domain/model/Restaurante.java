fbcbfa677770cecc128a0d74294f28c21618290a
Alex de Carvalho <leco_123carvalho@hotmail.com>
2022-10-13 22:44:51 -0300

    9.5. Conhecendo e adicionando mais constraints de validação no modelo https://docs.jboss.org/hibernate/stable/validator/reference/en-US/html_single/#section-builtin-constraints

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -7,7 +7,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -23,10 +23,11 @@ public class Restaurante {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
-	@NotNull
+	@NotBlank
 	@Column(length = 150, nullable = false)
 	private String nome;
 
+	@PositiveOrZero
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 
