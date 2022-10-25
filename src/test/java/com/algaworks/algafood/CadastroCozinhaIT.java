191b91d5c43e5ea04eafd929ca4f8a6c87839127
Alex de Carvalho <leco_123carvalho@hotmail.com>
2022-10-25 08:54:57 -0300

    10.12. Voltando o estado inicial do banco de dados para cada execução de teste com callback do Flyway

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -2,9 +2,11 @@ package com.algaworks.algafood;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.flywaydb.core.Flyway;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -22,12 +24,17 @@ public class CadastroCozinhaIT {
     @LocalServerPort
     private  int port;
 
+    @Autowired
+    private Flyway flyway;
+
     @Before
     public void setUP() {
         // Habilita para mostrar o erro de forma mais clara
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.port = port;
         RestAssured.basePath = "/cozinhas";
+
+        flyway.migrate();
     }
     @Test
     public void deveRetornarStatus200_QuandoConsultarConzinha() {
