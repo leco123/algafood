eb46f727ffdee9c2969f12350613007ddaea8b4a
leco123 <leco_carvalho@hotmail.com>
2022-11-02 20:09:25 -0300

    removendo os comentários

--- src/main/java/com/algaworks/algafood/api/controller/restaurante/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/restaurante/RestauranteController.java
@@ -57,42 +57,6 @@ public class RestauranteController {
         return listar();
     }
 
-//    @GetMapping
-//    public MappingJacksonValue listar(@RequestParam(required = false) String projecao) {
-//        List<Restaurante> restaurantes = restauranteRepository.findAll();
-//        List<RestauranteModel> restaurantesModels = restauranteModelAssembler.toCollectionModel(restaurantes);
-//
-//        MappingJacksonValue restaurantesWrapper = new MappingJacksonValue(restaurantesModels);
-//
-//        restaurantesWrapper.setSerializationView(RestauranteView.Resumo.class);
-//
-//        if ("apenas-nome".equals(projecao)) {
-//            restaurantesWrapper.setSerializationView(RestauranteView.ApenasNome.class);
-//        } else if ("completo".equals(projecao)) {
-//            restaurantesWrapper.setSerializationView(null);
-//        }
-//
-//        return restaurantesWrapper;
-//    }
-
-//    @GetMapping
-//    public List<RestauranteModel> listar() {
-//        return restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
-//    }
-//
-//    @JsonView(RestauranteView.Resumo.class)
-//    @GetMapping(params = "projecao=resumo")
-//    public List<RestauranteModel> listarResumido() {
-//        return listar();
-//    }
-//
-//    @JsonView(RestauranteView.ApenasNome.class)
-//    @GetMapping(params = "projecao=apenas-nome")
-//    public List<RestauranteModel> listarApenasNomes() {
-//        return listar();
-//    }
-
-
     @GetMapping("/{restauranteId}")
     public RestauranteModel buscar(@PathVariable Long restauranteId) {
         Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
