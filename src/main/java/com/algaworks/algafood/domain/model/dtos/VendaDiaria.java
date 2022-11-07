f5171caf1f31d05be8f452f507b84f187c399c24
leco123 <leco_carvalho@hotmail.com>
2022-11-07 15:30:50 -0300

    13.14. Implementando consulta com dados agregados de vendas diárias

--- src/main/java/com/algaworks/algafood/domain/model/dtos/VendaDiaria.java
+++ src/main/java/com/algaworks/algafood/domain/model/dtos/VendaDiaria.java
@@ -5,14 +5,14 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.util.Date;
 
 @AllArgsConstructor
 @Setter
 @Getter
 public class VendaDiaria {
 
-    private LocalDate data;
+    private Date data;
     private Long totalVendas;
     private BigDecimal totalFaturado;
 
--- src/main/java/com/algaworks/algafood/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/VendaQueryServiceImpl.java
@@ -1,15 +1,39 @@
 package com.algaworks.algafood.infrastructure.service;
 
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
+import com.algaworks.algafood.domain.model.Pedido;
 import com.algaworks.algafood.domain.model.dtos.VendaDiaria;
 import com.algaworks.algafood.domain.service.VendaQueryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+import javax.persistence.EntityManager;
+import java.time.LocalDate;
 import java.util.List;
 
+@Repository
 public class VendaQueryServiceImpl implements VendaQueryService {
+
+    @Autowired
+    private EntityManager manager;
 
     @Override
     public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro) {
-        return null;
+
+        var builder = manager.getCriteriaBuilder();
+        var query = builder.createQuery(VendaDiaria.class);
+        var root = query.from(Pedido.class);
+
+        // Função banco para converter Timestamp em data
+        var functionDateDataCriacao = builder.function("date", LocalDate.class, root.get("dataCriacao"));
+
+        var selection = builder.construct(VendaDiaria.class,
+                functionDateDataCriacao,
+                builder.count(root.get("id")),
+                builder.sum(root.get("valorTotal")));
+        query.select(selection);
+        query.groupBy(functionDateDataCriacao);
+
+        return manager.createQuery(query).getResultList();
     }
 }
