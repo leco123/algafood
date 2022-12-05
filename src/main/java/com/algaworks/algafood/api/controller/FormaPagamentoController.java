842f514c756bd2d3cb135cf3d5249b466a085b49
Alex de Carvalho <leco_123carvalho@hotmail.com>
2022-12-05 15:38:50 -0300

    17.3. Desafio: adicionando o cabeçalho Cache-Control na resposta

--- src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/FormaPagamentoController.java
@@ -45,10 +45,14 @@ public class FormaPagamentoController {
 	}
 
 	@GetMapping("/{formaPagamentoId}")
-	public FormaPagamentoModel buscar(@PathVariable Long formaPagamentoId) {
+	public ResponseEntity<FormaPagamentoModel> buscar(@PathVariable Long formaPagamentoId) {
 		FormaPagamento formaPagamento = cadastroFormaPagamento.buscarOuFalhar(formaPagamentoId);
 
-		return formaPagamentoModelAssembler.toModel(formaPagamento);
+		FormaPagamentoModel formaPagamentoModel =  formaPagamentoModelAssembler.toModel(formaPagamento);
+
+		return ResponseEntity.ok()
+				.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+				.body(formaPagamentoModel);
 	}
 
 	@PostMapping
