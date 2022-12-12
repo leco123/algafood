d93a5c3a3219492a19a7b79e792586967507f245
Alex de Carvalho <leco_123carvalho@hotmail.com>
2022-12-12 17:26:13 -0300

    18.27

--- src/main/java/com/algaworks/algafood/api/openapi/model/PageModelOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/openapi/model/PageModelOpenApi.java
@@ -1,6 +1,5 @@
 package com.algaworks.algafood.api.openapi.model;
 
-import com.algaworks.algafood.api.model.cozinha.CozinhaModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
