73a4a7e630b9e1ca99ca7238ecad3beb42b1bb32
Alex de Carvalho <leco_123carvalho@hotmail.com>
2022-12-12 21:57:39 -0300

    18.37. Desafio: descrevendo documentação de endpoints de associação de permissões com grupos

--- src/main/java/com/algaworks/algafood/api/controller/grupo/GrupoPermissaoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/grupo/GrupoPermissaoController.java
@@ -2,17 +2,20 @@ package com.algaworks.algafood.api.controller.grupo;
 
 import com.algaworks.algafood.api.assembler.usuario.permissao.PermissaoModelAssembler;
 import com.algaworks.algafood.api.model.usuario.permissao.PermissaoModel;
+import com.algaworks.algafood.api.openapi.controller.GrupoPermissaoControllerOpenApi;
 import com.algaworks.algafood.domain.model.Grupo;
 import com.algaworks.algafood.domain.service.CadastroGrupoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/grupos/{grupoId}/permissoes")
-public class GrupoPermissaoController {
+@RequestMapping(path = "/grupos/{grupoId}/permissoes",
+        produces = MediaType.APPLICATION_JSON_VALUE)
+public class GrupoPermissaoController implements GrupoPermissaoControllerOpenApi {
     @Autowired
     private CadastroGrupoService cadastroGrupo;
 
--- src/main/java/com/algaworks/algafood/api/model/usuario/permissao/PermissaoModel.java
+++ src/main/java/com/algaworks/algafood/api/model/usuario/permissao/PermissaoModel.java
@@ -1,12 +1,19 @@
 package com.algaworks.algafood.api.model.usuario.permissao;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class PermissaoModel {
+
+    @ApiModelProperty(example = "1")
     private Long id;
+
+    @ApiModelProperty(example = "CONSULTAR_COZINHAS")
     private String nome;
+
+    @ApiModelProperty(example = "Permite consultar cozinhas")
     private String descricao;
 }
