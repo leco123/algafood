55d7e41ed85caa1c7c8c22d0527661ae261d21c9
Alex de Carvalho <leco_123carvalho@hotmail.com>
2022-12-12 21:26:27 -0300

    18.33. Desafio: descrevendo documentação de endpoints de associação de restaurantes com usuários

--- src/main/java/com/algaworks/algafood/api/controller/usuario/RestauranteUsuarioResponsavelController.java
+++ src/main/java/com/algaworks/algafood/api/controller/usuario/RestauranteUsuarioResponsavelController.java
@@ -2,17 +2,19 @@ package com.algaworks.algafood.api.controller.usuario;
 
 import com.algaworks.algafood.api.assembler.usuario.UsuarioModelAssembler;
 import com.algaworks.algafood.api.model.usuario.UsuarioModel;
+import com.algaworks.algafood.api.openapi.controller.RestauranteUsuarioResponsavelControllerOpenApi;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/restaurantes/{restauranteId}/responsaveis")
-public class RestauranteUsuarioResponsavelController {
+@RequestMapping(path = "/restaurantes/{restauranteId}/responsaveis", produces = MediaType.APPLICATION_JSON_VALUE)
+public class RestauranteUsuarioResponsavelController implements RestauranteUsuarioResponsavelControllerOpenApi {
 
     @Autowired
     private CadastroRestauranteService cadastroRestaurante;
