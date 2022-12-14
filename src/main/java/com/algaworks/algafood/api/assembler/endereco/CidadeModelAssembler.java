78f942f70978e8904df30f2188ce27dcfdf33123
Alex de Carvalho <leco_123carvalho@hotmail.com>
2022-12-14 14:37:14 -0300

    19.11. Montando modelo de representação com RepresentationModelAssembler

--- src/main/java/com/algaworks/algafood/api/assembler/endereco/CidadeModelAssembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/endereco/CidadeModelAssembler.java
@@ -1,27 +1,98 @@
 package com.algaworks.algafood.api.assembler.endereco;
 
+import com.algaworks.algafood.api.controller.endereco.CidadeController;
+import com.algaworks.algafood.api.controller.endereco.EstadoController;
 import com.algaworks.algafood.api.model.endereco.cidade.CidadeModel;
 import com.algaworks.algafood.domain.model.Cidade;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-import java.util.stream.Collectors;
+import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.methodOn;
 
 @Component
-public class CidadeModelAssembler {
+public class CidadeModelAssembler extends RepresentationModelAssemblerSupport<Cidade, CidadeModel> {
 
     @Autowired
     private ModelMapper modelMapper;
 
-    public CidadeModel toModel(Cidade cidade) {
-        return modelMapper.map(cidade, CidadeModel.class);
+    /**
+     * Cria um novo {@link RepresentationModelAssemblerSupport} usando a classe de controlador
+     * e o tipo de recurso fornecidos.
+     *
+     * @param controllerClass não deve ser {@literal null}.
+     * @param resourceType não deve ser {@literal null}.
+     */
+    public CidadeModelAssembler(Class<?> controllerClass, Class<CidadeModel> resourceType) {
+        super(controllerClass, resourceType);
     }
 
-    public List<CidadeModel> toCollectionModel(List<Cidade> cidades) {
-        return cidades.stream()
-                .map(cidade -> toModel(cidade))
-                .collect(Collectors.toList());
+    /**
+     * methodOn, tem a finalidade de criar um proxy que intercepta as chamadas, entenda como:
+     *
+     * Quando é feito a chamada ao método buscar esta sendo chamado o buscar do proxy e não do CidadeController real.
+     * o proxy é como se fosse um clone da classe que estamos usando CidadeController.class, porém usado apenas
+     * para pegar os metadados vamos dizer assim.
+     *
+     * isso evita que faça/execute chamadas desnecessárias como em nosso caso precisamos apenas pegar a url isso ajuda
+     * o sistema não ficar usando ou executando recursos desnecessários, de forma resumida o proxy vai
+     * a chamada e não vai passar para a frente para CidadeController, mas vai registra um histórico... sendo assim
+     * o proxy está apenas registrando a chamada em um histórico para registra o link
+     *
+     * Veja explicação aqui: 19.9. Construindo links que apontam para métodos
+     */
+    @Override
+    public CidadeModel toModel(Cidade cidade) {
+
+        CidadeModel cidadeModel = createModelWithId(cidade.getId(), cidade);
+
+        modelMapper.map(cidade, cidadeModel);
+
+//      CidadeModel cidadeModel = modelMapper.map(cidade, CidadeModel.class);
+
+//      http://localhost:8080/cidades/1
+//      [antes como estava implementado]
+//      cidadeModel.add(WebMvcLinkBuilder.linkTo(CidadeController.class).slash(cidadeModel.getId()).withSelfRel());
+
+//      cidadeModel.add(WebMvcLinkBuilder
+//           .linkTo(methodOn(CidadeController.class)
+//              .buscar(cidadeModel.getId()))
+//           .withSelfRel());
+
+//      http://localhost:8080/cidades
+//      [antes como estava implementado]
+//      cidadeModel.add(WebMvcLinkBuilder.linkTo(CidadeController.class).withRel("cidades"));
+        cidadeModel.add(WebMvcLinkBuilder
+                .linkTo(methodOn(CidadeController.class)
+                        .listar())
+                .withRel("cidades"));
+
+//      http://localhost:8080/estados/1
+//      [antes como estava implementado]
+//      cidadeModel.add(WebMvcLinkBuilder.linkTo(EstadoController.class).slash(cidadeModel.getEstado().getId()).withSelfRel());
+        cidadeModel.add(WebMvcLinkBuilder
+                .linkTo(methodOn(EstadoController.class)
+                        .buscar(cidadeModel.getId()))
+                .withSelfRel());
+
+        return cidadeModel;
     }
+
+    @Override
+    public CollectionModel<CidadeModel> toCollectionModel(Iterable<? extends Cidade> entities) {
+        return super.toCollectionModel(entities)
+                .add(WebMvcLinkBuilder.linkTo(CidadeController.class)
+                        .withSelfRel());
+    }
+
+
+//    public List<CidadeModel> toCollectionModel(List<Cidade> cidades) {
+//        return cidades.stream()
+//                .map(cidade -> toModel(cidade))
+//                .collect(Collectors.toList());
+//    }
+
 }
--- src/main/java/com/algaworks/algafood/api/controller/endereco/CidadeController.java
+++ src/main/java/com/algaworks/algafood/api/controller/endereco/CidadeController.java
@@ -14,15 +14,12 @@ import com.algaworks.algafood.domain.service.CadastroCidadeService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-
-import static org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping(path = "/cidades", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -44,90 +41,14 @@ public class CidadeController implements CidadeControllerOpenApi {
 	@GetMapping
 	public CollectionModel<CidadeModel> listar() {
 		List<Cidade> todasCidades = cidadeRepository.findAll();
-		List<CidadeModel> cidadesModel = cidadeModelAssembler.toCollectionModel(todasCidades);
-
-		CollectionModel<CidadeModel> cidadesCollectionModel = new CollectionModel<>(cidadesModel);
-
-		cidadesModel.forEach(cidadeModel -> {
-			// http://localhost:8080/cidades/1
-			// [antes como estava implementado]
-			// cidadeModel.add(WebMvcLinkBuilder.linkTo(CidadeController.class).slash(cidadeModel.getId()).withSelfRel());
-			cidadeModel.add(WebMvcLinkBuilder
-					.linkTo(methodOn(CidadeController.class)
-							.buscar(cidadeModel.getId()))
-					.withSelfRel());
-
-			// http://localhost:8080/cidades
-			// [antes como estava implementado]
-			// cidadeModel.add(WebMvcLinkBuilder.linkTo(CidadeController.class).withRel("cidades"));
-			cidadeModel.add(WebMvcLinkBuilder
-					.linkTo(methodOn(CidadeController.class)
-							.listar())
-					.withRel("cidades"));
-
-			// http://localhost:8080/estados/1
-			// [antes como estava implementado]
-			//cidadeModel.add(WebMvcLinkBuilder.linkTo(EstadoController.class).slash(cidadeModel.getEstado().getId()).withSelfRel());
-			cidadeModel.add(WebMvcLinkBuilder
-					.linkTo(methodOn(EstadoController.class)
-							.buscar(cidadeModel.getId()))
-					.withSelfRel());
-		});
-
-		cidadesCollectionModel.add(WebMvcLinkBuilder
-				.linkTo(CidadeController.class)
-				.withSelfRel());
-
-		return cidadesCollectionModel;
+		return cidadeModelAssembler.toCollectionModel(todasCidades);
 	}
 
 	@GetMapping("/{cidadeId}")
 	public CidadeModel buscar(
 			@PathVariable Long cidadeId) {
 		Cidade cidade = cadastroCidade.buscarOuFalhar(cidadeId);
-
-		CidadeModel cidadeModel = cidadeModelAssembler.toModel(cidade);
-
-		/**
-		 * methodOn, tem a finalidade de criar um proxy que intercepta as chamadas, entenda como:
-		 *
-		 * Quando é feito a chamada ao método buscar esta sendo chamado o buscar do proxy e não do CidadeController real.
-		 * o proxy é como se fosse um clone da classe que estamos usando CidadeController.class, porém usado apenas
-		 * para pegar os metadados vamos dizer assim.
-		 *
-		 * isso evita que faça/execute chamadas desnecessárias como em nosso caso precisamos apenas pegar a url isso ajuda
-		 * o sistema não ficar usando ou executando recursos desnecessários, de forma resumida o proxy vai
-		 * a chamada e não vai passar para a frente para CidadeController, mas vai registra um histórico... sendo assim
-		 * o proxy está apenas registrando a chamada em um histórico para registra o link
-		 *
-		 * Veja explicação aqui: 19.9. Construindo links que apontam para métodos
-		 */
-
-		// http://localhost:8080/cidades/1
-		// [antes como estava implementado]
-		// cidadeModel.add(WebMvcLinkBuilder.linkTo(CidadeController.class).slash(cidadeModel.getId()).withSelfRel());
-		cidadeModel.add(WebMvcLinkBuilder
-				.linkTo(methodOn(CidadeController.class)
-						.buscar(cidadeModel.getId()))
-				.withSelfRel());
-
-		// http://localhost:8080/cidades
-		// [antes como estava implementado]
-		// cidadeModel.add(WebMvcLinkBuilder.linkTo(CidadeController.class).withRel("cidades"));
-		cidadeModel.add(WebMvcLinkBuilder
-				.linkTo(methodOn(CidadeController.class)
-						.listar())
-				.withRel("cidades"));
-
-		// http://localhost:8080/estados/1
-		// [antes como estava implementado]
-		//cidadeModel.add(WebMvcLinkBuilder.linkTo(EstadoController.class).slash(cidadeModel.getEstado().getId()).withSelfRel());
-		cidadeModel.add(WebMvcLinkBuilder
-				.linkTo(methodOn(EstadoController.class)
-						.buscar(cidadeModel.getId()))
-				.withSelfRel());
-
-		return cidadeModel;
+		return cidadeModelAssembler.toModel(cidade);
 	}
 
 	@PostMapping
