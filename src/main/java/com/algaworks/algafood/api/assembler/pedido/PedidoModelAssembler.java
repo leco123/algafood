fd49df41d5861a4c87cc185ca660870e6d45866a
Alex de Carvalho <leco_123carvalho@hotmail.com>
2022-12-16 11:06:24 -0300

    19.18. Adicionando links com template variables

--- src/main/java/com/algaworks/algafood/api/assembler/pedido/PedidoModelAssembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/pedido/PedidoModelAssembler.java
@@ -10,6 +10,10 @@ import com.algaworks.algafood.api.model.pedido.PedidoModel;
 import com.algaworks.algafood.domain.model.Pedido;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.TemplateVariable;
+import org.springframework.hateoas.TemplateVariables;
+import org.springframework.hateoas.UriTemplate;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Component;
@@ -30,7 +34,19 @@ public class PedidoModelAssembler
         PedidoModel pedidoModel = createModelWithId(pedido.getCodigo(), pedido);
         modelMapper.map(pedido, pedidoModel);
 
-        pedidoModel.add(WebMvcLinkBuilder.linkTo(PedidoController.class).withRel("pedidos"));
+        TemplateVariables pageVariables = new TemplateVariables(
+                new TemplateVariable("page", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("size", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("sort", TemplateVariable.VariableType.REQUEST_PARAM)
+        );
+
+        String pedidoUrl = WebMvcLinkBuilder.linkTo(PedidoController.class).toUri().toString();
+
+//        pedidoModel.add(WebMvcLinkBuilder.linkTo(PedidoController.class).withRel("pedidos"));
+//       UriTemplate, recebe 2(dois) parâmetros
+//       1º urlString exemplo "http://localhost:8080/pedidos"
+//       2º TemplateVariables variaveis da página ex: {"page","sort","size"... outros}
+        pedidoModel.add(new Link(UriTemplate.of(pedidoUrl, pageVariables), "pedidos"));
 
         pedidoModel.getRestaurante().add(WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(RestauranteController.class)
                 .buscar(pedido.getRestaurante().getId())).withSelfRel());
