cad83a1fb6e51092f9ed6587a6d7a265a5f9167d
Alex de Carvalho <leco_123carvalho@hotmail.com>
2022-12-17 13:21:26 -0300

    19.41. Corrigindo a paginação na documentação

--- src/main/java/com/algaworks/algafood/api/openapi/model/CozinhasModelOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/openapi/model/CozinhasModelOpenApi.java
@@ -2,10 +2,24 @@ package com.algaworks.algafood.api.openapi.model;
 
 import com.algaworks.algafood.api.model.cozinha.CozinhaModel;
 import io.swagger.annotations.ApiModel;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.hateoas.Links;
+
+import java.util.List;
 
 @ApiModel("CozinhasModel")
-public class CozinhasModelOpenApi extends PageModelOpenApi<CozinhaModel> {
+@Setter
+@Getter
+public class CozinhasModelOpenApi {
+    private CozinhaEmbeddedModelOpenApi _embedded;
+    private Links _links;
+    private PageModelOpenApi page;
 
+    @ApiModel("CozinhasEmbeddedModel")
+    @Data
+    public class CozinhaEmbeddedModelOpenApi {
+        private List<CozinhaModel> cozinhas;
+    }
 }
--- src/main/java/com/algaworks/algafood/api/openapi/model/PageModelOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/openapi/model/PageModelOpenApi.java
@@ -1,11 +1,13 @@
 package com.algaworks.algafood.api.openapi.model;
 
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
 
+@ApiModel("PageModel")
 @Getter
 @Setter
 public class PageModelOpenApi<T> {
