0e5101b8bbf4cdb628e8492872029feaa93eb8a3
Alex de Carvalho <leco_123carvalho@hotmail.com>
2022-12-17 13:30:22 -0300

    19.45. Desafio: corrigindo a documentação dos endpoint de pedidos (paginação)

--- src/main/java/com/algaworks/algafood/api/openapi/model/PedidosResumoModelOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/openapi/model/PedidosResumoModelOpenApi.java
@@ -2,7 +2,27 @@ package com.algaworks.algafood.api.openapi.model;
 
 import com.algaworks.algafood.api.model.pedido.PedidoResumoModel;
 import io.swagger.annotations.ApiModel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.hateoas.Links;
+
+import java.util.List;
 
 @ApiModel("PedidosResumoModel")
-public class PedidosResumoModelOpenApi  extends PageModelOpenApi<PedidoResumoModel> {
+@Getter
+@Setter
+public class PedidosResumoModelOpenApi {
+
+    private PedidosResumoEmbeddedModelOpenApi _embedded;
+    private Links _links;
+    private PageModelOpenApi page;
+
+    @ApiModel("PedidosResumoEmbeddedModel")
+    @Data
+    public class PedidosResumoEmbeddedModelOpenApi {
+
+        private List<PedidoResumoModel> pedidos;
+
+    }
 }
