cf54c7824d2d40b19311ac7a6c61d440403c96f2
Alex de Carvalho <leco_123carvalho@hotmail.com>
2022-12-19 16:50:08 -0300

    22.4. Configurando autenticação de usuários em memória

--- src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/WebSecurityConfig.java
@@ -1,14 +1,32 @@
 package com.algaworks.algafood.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure (AuthenticationManagerBuilder auth) throws Exception {
+        // Configurar em memória
+        auth.inMemoryAuthentication()
+            .withUser("alex")
+                    .password(passwordEncoder().encode("123"))
+                    .roles("ADMIN")
+            .and()
+            .withUser("joao")
+                .password(passwordEncoder().encode("123"))
+                .roles("ADMIN");
+    }
+
     @Override
     protected void configure (HttpSecurity http) throws Exception {
         http.httpBasic()
@@ -26,5 +44,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     // Desabilitar CSRF
                     .csrf().disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        // método de criptografia, para incriptografar as senhas
+        return new BCryptPasswordEncoder();
     }
 }
