9ba14f1b722fe2fc8ba51adba86a8187f0ea2e8d
Alex de Carvalho <leco_123carvalho@hotmail.com>
2022-12-22 15:44:19 -0300

    23.6. Configurando o Resource Server para JWT assinado com chave simétrica

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,9 +1,14 @@
 package com.algaworks.algafood.core.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebSecurity
@@ -17,9 +22,19 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
                 .and()
                     .cors().
                 and()
-                // Oauth Resource emite um opaqueToken, é um chave que não da pra identificar oque existe dentro dela
-                .oauth2ResourceServer().opaqueToken();
+                    // Oauth Resource emite um opaqueToken, é um chave que não da pra identificar oque existe dentro dela
+                    .oauth2ResourceServer()
+                    //.opaqueToken()
+                    .jwt();
+    }
 
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        var secretKey = new SecretKeySpec("algaworkssdfsdf878451548748454f5asdfasdas".getBytes(), "HmacSHA256");
+
+        return NimbusJwtDecoder
+                .withSecretKey(secretKey)
+                .build();
     }
 
 }
