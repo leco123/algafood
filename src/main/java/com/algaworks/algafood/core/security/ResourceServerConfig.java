b695b29355d767660eeba1d1fed10355b38caaf7
Alex de Carvalho <leco_123carvalho@hotmail.com>
2022-12-26 16:30:14 -0300

    23.20. Carregando as Granted Authorities e restringindo acesso a endpoints na API

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -1,9 +1,18 @@
 package com.algaworks.algafood.core.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
@@ -11,16 +20,38 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure (HttpSecurity http) throws Exception {
+        Converter<Jwt, ? extends AbstractAuthenticationToken> jwtAuthenticationConverter;
         http
                 .authorizeRequests()
-                    .anyRequest().authenticated()
+                    .antMatchers(HttpMethod.POST, "/v1/cozinhas/**").hasAnyAuthority("EDITAR_COZINHAS")
+                    .antMatchers(HttpMethod.PUT, "/v1/cozinhas/**").hasAnyAuthority("EDITAR_COZINHAS")
+                    .antMatchers(HttpMethod.GET, "/v1/cozinhas/**").authenticated()
+                    //.anyRequest().authenticated()
+                    .anyRequest().denyAll()
                 .and()
-                    .cors().
-                and()
-                    // Oauth Resource emite um opaqueToken, é um chave que não da pra identificar oque existe dentro dela
-                    .oauth2ResourceServer()
-                    //.opaqueToken()
-                    .jwt();
+                .cors().and()
+                // Oauth Resource emite um opaqueToken, é um chave que não da pra identificar oque existe dentro dela
+                //.opaqueToken()
+                .oauth2ResourceServer()
+                    .jwt()
+                    .jwtAuthenticationConverter(jwtAuthenticationConverter());
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter () {
+        var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+
+            var authorities = jwt.getClaimAsStringList("authorities");
+
+            if (authorities == null) {
+                authorities = Collections.emptyList();
+            }
+            return authorities.stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        });
+
+        return jwtAuthenticationConverter;
     }
 
 //    @Bean
