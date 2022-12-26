3b333c68e756cdbafe60ba8d4a4e05172581a0f9
Alex de Carvalho <leco_123carvalho@hotmail.com>
2022-12-26 10:40:35 -0300

    23.16. Adicionando claims customizadas no payload do JWT

--- src/main/java/com/algaworks/algafood/domain/model/Usuario.java
+++ src/main/java/com/algaworks/algafood/domain/model/Usuario.java
@@ -6,9 +6,7 @@ import org.hibernate.annotations.CreationTimestamp;
 
 import javax.persistence.*;
 import java.time.OffsetDateTime;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 @Data
@@ -41,12 +39,8 @@ public class Usuario {
     )
     private Set<Grupo> grupos = new HashSet<>();
 
-    public boolean senhaCoincideCom(String senha) {
-        return getSenha().equals(senha);
-    }
-
-    public boolean senhaNaoCoincideCom(String senha) {
-        return !senhaCoincideCom(senha);
+    public boolean isNovo() {
+        return getId() == null;
     }
 
     public boolean removerGrupo(Grupo grupo) {
--- src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
@@ -6,6 +6,7 @@ import com.algaworks.algafood.domain.model.Grupo;
 import com.algaworks.algafood.domain.model.Usuario;
 import com.algaworks.algafood.domain.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -20,13 +21,24 @@ public class CadastroUsuarioService {
     @Autowired
     private CadastroGrupoService cadastroGrupo;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Transactional
     public Usuario salvar(Usuario usuario) {
         usuarioRepository.detach(usuario);
+
         Optional<Usuario> usuarioExistente = usuarioRepository.findByEmail(usuario.getEmail());
+
         if (usuarioExistente.isPresent() && !usuarioExistente.get().equals(usuario)) {
-            throw new NegocioException(String.format("Já existe um usuário cadastrado com o e-mail %s", usuario.getEmail()));
+            throw new NegocioException(
+                    String.format("Já existe um usuário cadastrado com o e-mail %s", usuario.getEmail()));
         }
+
+        if (usuario.isNovo()) {
+            usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
+        }
+
         return usuarioRepository.save(usuario);
     }
 
@@ -34,12 +46,13 @@ public class CadastroUsuarioService {
     public void alterarSenha(Long usuarioId, String senhaAtual, String novaSenha) {
         Usuario usuario = buscarOuFalhar(usuarioId);
 
-        if (usuario.senhaNaoCoincideCom(senhaAtual)) {
+        if (!passwordEncoder.matches(senhaAtual, usuario.getSenha())) {
             throw new NegocioException("Senha atual informada não coincide com a senha do usuário.");
         }
 
-        usuario.setSenha(novaSenha);
+        usuario.setSenha(passwordEncoder.encode(novaSenha));
     }
+
     @Transactional
     public void desassociarGrupo(Long usuarioId, Long grupoId) {
         Usuario usuario = buscarOuFalhar(usuarioId);
