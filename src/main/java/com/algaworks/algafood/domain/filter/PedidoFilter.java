d0d096259bdad435a28a0ab6bc9f69cb55a46a26
Alex de Carvalho <leco_123carvalho@hotmail.com>
2025-06-06 07:25:19 -0300

    Update comment path for PedidoController

--- src/main/java/com/algaworks/algafood/domain/filter/PedidoFilter.java
+++ src/main/java/com/algaworks/algafood/domain/filter/PedidoFilter.java
@@ -10,7 +10,7 @@ import java.time.OffsetDateTime;
 /**
  * Quando usar pesquisa por data e hora de passar a annotation @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
  * para evitar erro de formatação de data e hora o spring não consegue reconhecer o formato string
- * ERRO: org.springframework.beans.factory.BeanCreationException: Error creating bean with name 'pedidoController': Lookup method resolution failed; nested exception is java.lang.IllegalStateException: Failed to introspect Class [com.algaworks.algafood.api.controller.pedido.PedidoController] from ClassLoader [org.springframework.boot.devtools.restart.classloader.RestartClassLoader@2643d422]
+ * ERRO: org.springframework.beans.factory.BeanCreationException: Error creating bean with name 'pedidoController': Lookup method resolution failed; nested exception is java.lang.IllegalStateException: Failed to introspect Class [com.algaworks.algafood.api.v1.controller.pedido.PedidoController] from ClassLoader [org.springframework.boot.devtools.restart.classloader.RestartClassLoader@2643d422]
  */
 @Getter
 @Setter
