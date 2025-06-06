2efef22f896df1a9c95bec7632e90f80e0b58c2b
Alex de Carvalho <leco_123carvalho@hotmail.com>
2025-06-06 07:17:12 -0300

    Fix duplicated join in RestauranteRepository query

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -14,7 +14,7 @@ public interface RestauranteRepository extends
         RestauranteRepositoryQueries,
         JpaSpecificationExecutor<Restaurante> {
 
-    @Query("from Restaurante r join r.cozinha join fetch r.cozinha")
+    @Query("from Restaurante r join fetch r.cozinha")
     List<Restaurante> findAll();
 
     List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
